defa600e2858326326f1f9d015a8f25ffe4913cc
Islam <isxmad@gmail.com>
2021-11-17 03:15:26 +0300

    fgssdf

--- #src/js/script.js
+++ #src/js/script.js
@@ -201,39 +201,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	/*Yandex Map*/
-	// ymaps.ready(function () {
- //        let myMap = new ymaps.Map('map', {
- //                center: [51.118834, 71.483385],
- //                zoom: 17
- //            }, {
- //                searchControlProvider: 'yandex#search'
- //            }),
+	ymaps.ready(function () {
+        let myMap = new ymaps.Map('map', {
+                center: [51.118834, 71.483385],
+                zoom: 17
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
 
- //            // Создаём макет содержимого.
- //            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
- //                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
- //            ),
+            // Создаём макет содержимого.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
 
- //            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
- //                hintContent: 'Собственный значок метки',
- //                balloonContent: 'Это красивая метка'
- //            }, {
- //                // Опции.
- //                // Необходимо указать данный тип макета.
- //                iconLayout: 'default#image',
- //                // Своё изображение иконки метки.
- //                iconImageHref: 'https://erzhanpvl.github.io/img/mapicon.png',
- //                // Размеры метки.
- //                iconImageSize: [100, 96],
- //                // Смещение левого верхнего угла иконки относительно
- //                // её "ножки" (точки привязки).
- //                iconImageOffset: [-100, -38]
- //            })
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Собственный значок метки',
+                balloonContent: 'Это красивая метка'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'https://erzhanpvl.github.io/img/mapicon.png',
+                // Размеры метки.
+                iconImageSize: [100, 96],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-100, -38]
+            })
 
- //        myMap.geoObjects
- //            .add(myPlacemark)
- //            .add(myPlacemarkWithContent);
- //    });
+        myMap.geoObjects
+            .add(myPlacemark)
+            
+    });
 })
 
 
